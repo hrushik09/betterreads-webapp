df342cf5f09db40b21ea3e370fc5c75505787e9f
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2022-05-18 23:34:25 +0530

    added BookController.java and Book page

---
+++ src/main/java/io/hrushik09/betterreads/BetterReadsApp.java
@@ -0,0 +1,35 @@
+package io.hrushik09.betterreads;
+
+import io.hrushik09.betterreads.connection.DataStaxAstraProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
+
+@SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
+public class BetterReadsApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterReadsApp.class, args);
+    }
+
+//    @RequestMapping("/user")
+//    public String user(@AuthenticationPrincipal OAuth2User principal) {
+//        System.out.println(principal);
+//        return principal.getAttribute("name");
+//    }
+
+    @Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
+}
